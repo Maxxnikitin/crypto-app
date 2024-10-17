138fc789b8adf54af079d36d3fe63a6946be8fe5
Maxx Nikitin <maxxnikitin90@yandex.ru>
2024-10-17 13:39:55 +0700

    fix: fix tg connecting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
+"use client";
+
+import { useTelegram } from "./hooks/use-telegram";
 import styles from "./page.module.css";
-import { Telegram } from "./components/telegram";
 
 export default function Home() {
+  useTelegram();
+
   return (
     <div className={styles.page}>
       <h1>Привет, это интеграция с Telegram</h1>
       <p>Здесь будет информация, связанная с пользователем.</p>
-      <Telegram />
     </div>
   );
 }
